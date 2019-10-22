28bd043464877f2548777367f639004a83cefa45
Boris Nekezov <boris.nekezov@gmail.com>
2019-10-22 18:01:29 +0300

    Refactor code in controls

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -4,31 +4,32 @@ import classes from './Controls.module.css';
 class Controls extends Component {
     render() {
         const { Button, StartGame, Attack, SpecialAttack, Heal, GiveUp, Controls } = classes;
-
+        const { start, attacked, healed, gaveUp } = this.props;
         let gameControls = (
-                <button 
-                    className={`${Button} ${StartGame}`}
-                    onClick={this.props.start}>START NEW GAME</button>
+            <button 
+                className={`${Button} ${StartGame}`}
+                onClick={start}>START NEW GAME</button>
         );
         // if gameStarted is true start game markup
         if (this.props.gameStarted){
             gameControls = (
                 <div className="small-12 columns">
+                    {/* atack */}
                     <button 
                         className={`${Button} ${Attack}`}
-                        onClick={() => this.props.attacked(3, 10)}>ATTACK</button> 
-                    
+                        onClick={() => attacked(3, 10)}>ATTACK</button> 
+                    {/* special attack */}
                     <button 
                         className={`${Button} ${SpecialAttack}`}
-                        onClick={() => this.props.attacked(5, 12)}>SPECIAL ATTACK</button> 
-                    
+                        onClick={() => attacked(5, 12)}>SPECIAL ATTACK</button>
+                    {/* heal */}
                     <button 
                         className={`${Button} ${Heal}`}
-                        onClick={this.props.healed}>HEAL</button> 
-                    
+                        onClick={healed}>HEAL</button> 
+                    {/* give up */}
                     <button 
                         className={`${Button} ${GiveUp}`}
-                        onClick={this.props.gaveUp}>GIVE UP</button>
+                        onClick={gaveUp}>GIVE UP</button>
                 </div>
             );
         } 
