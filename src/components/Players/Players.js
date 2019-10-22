806f28f2f9da69155b22fee92b18010e92ac68f2
Boris Nekezov <boris.nekezov@gmail.com>
2019-10-22 18:45:01 +0300

    Refactor code

--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -2,17 +2,17 @@ import React,  { Component } from 'react';
 import HealthBar from './HealthBar/HealthBar';
 
 class Players extends Component {
- 
     render(){
+        const { playerHP, monsterHP } = this.props;
         return (    
             <section className="row">
                 <div className="small-6 columns">
                     <h1 className="text-center">YOU</h1>
-                    <HealthBar healthPoints={this.props.playerHP} />   
+                    <HealthBar healthPoints={playerHP} />   
                 </div>
                 <div className="small-6 columns">
                     <h1 className="text-center">MONSTER</h1>
-                    <HealthBar healthPoints={this.props.monsterHP} />
+                    <HealthBar healthPoints={monsterHP} />
                 </div>
             </section>
         );
